c85b0f43916620fddd4b3dfb642b7ecc787a9729
foysalious <foysalrahman112@gmail.com>
2020-07-11 04:20:34 +0600

    2nd commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const mysql=require('mysql');
+const express=require('express');
+var app= express();
+const bodyparser= require('body-parser');
+app.use(bodyparser.json());
+
+var mysqlConnection = mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  password:'',
+  database: 'employeedb'
+});
+mysqlConnection.connect((err)=>{
+  if(!err)
+  console.log('DB Connected');
+  else {
+    console.log('Connection Failed'+JSON.stringfy(err,undefined,2));
+  }
+});
+
+app.listen(3000,()=>console.log('Express Server is Running  at port no :3000'));
+
+app.get('/employees',(req,res)=>{
+  mysqlConnection.query('Select * form employee',(err,rows,fields)=>{
+    if (!err)
+      console.log(rows);
+
+    else
+      console.log(err);
+
+  })
+});

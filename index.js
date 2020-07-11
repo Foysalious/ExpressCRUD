861eb97174754b4bf3de5b8431b3c60e0fee0167
foysalious <foysalrahman112@gmail.com>
2020-07-11 16:18:34 +0600

    git commit -m "update"

--- index.js
+++ index.js
@@ -20,10 +20,23 @@ mysqlConnection.connect((err)=>{
 
 app.listen(3000,()=>console.log('Express Server is Running  at port no :3000'));
 
+//Get All Employee
 app.get('/employees',(req,res)=>{
   mysqlConnection.query('SELECT * From employee',(err,rows,fields)=>{
     if (!err)
-      console.log(rows);
+      res.send(rows);
+
+    else
+      console.log(err);
+
+  })
+});
+
+//Get an Employees
+app.get('/employees/:id',(req,res)=>{
+  mysqlConnection.query('SELECT * From employee where EmpID=?',[req.params.id],(err,rows,fields)=>{
+    if (!err)
+      res.send(rows);
 
     else
       console.log(err);
